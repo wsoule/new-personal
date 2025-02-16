3b4b2c7177fee72ba82916946a8e171e50b8f9c0
Wyat Soule <wsoule679@gmail.com>
2025-02-15 23:16:11 -0500

    feat: add tailwind

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Wyat'
+export const description = 'Wyat\'s website';
+export const url = dev ? 'http://localhost:5173/' : 'https://wyat.me'
